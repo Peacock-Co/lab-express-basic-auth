b6cf084896b74c1eef171a3bbcb538a6a196430d
Peacock-Co <pavaoleonardo@hotmail.com>
2020-02-05 18:31:57 +0100

    Finished iteration 3

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('auth');
+router.get("/", (req, res, next) => {
+  res.render("auth");
 });
-
 
 // routes/auth.js
 
@@ -15,7 +14,6 @@ const User = require("../models/user");
 // BCrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-
 
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
@@ -31,7 +29,7 @@ router.post("/signup", (req, res, next) => {
   }
 
   User.findOne({
-      "username": username
+      username: username
     })
     .then(user => {
       if (user !== null) {
@@ -53,16 +51,57 @@ router.post("/signup", (req, res, next) => {
         })
         .catch(error => {
           console.log(error);
-        })
+        });
     })
     .catch(error => {
-      next(error)
-    })
+      next(error);
+    });
 });
 
 // GET Auth Page
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({
+      username: theUsername
+    })
+    .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+
 });
 
 module.exports = router;
